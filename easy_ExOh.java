22e86ef715d047e7058cf4c61a4481db75b8e297
Gabriel G <GabrielGhe@users.noreply.github.com>
2014-04-08 22:07:14 -0400

    Update easy_ExOh.java

--- easy_ExOh.java
+++ easy_ExOh.java
@@ -7,13 +7,13 @@ public class Easy_ExOh {
 	
 	public static boolean function(String input){
 		Map<String, Integer> map = new HashMap<String, Integer>();
-		map.put("x", 0);
+		map.put("x", 0);//set initial values inside hashmap
 		map.put("o", 0);
-		for(char val : input.toCharArray()){
-			String x = String.valueOf(val).toLowerCase();
-			map.put(x, map.get(x) + 1);
+		for(char val : input.toCharArray()){//iterate through the string
+			String x = String.valueOf(val).toLowerCase(); //converting each character to lower case
+			map.put(x, map.get(x) + 1);//and incrementing its value inside the map
 		}
-		return map.get("x") == map.get("o");
+		return map.get("x") == map.get("o");//find out if the x and o values are equal
 	}
 
 	public static void main(String[] args) {
