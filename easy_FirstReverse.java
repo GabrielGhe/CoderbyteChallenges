268efcd919d463290fff42d9b13472ea06235b69
Gabriel G <GabrielGhe@users.noreply.github.com>
2014-04-13 20:30:21 -0400

    Update easy_FirstReverse.java
    Added the reason why I didn't write the solution on just 1 line

--- easy_FirstReverse.java
+++ easy_FirstReverse.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Easy_FirstReverse {
-
+	// all of this can be done on 1 line;however, it looks too complicated and cluttered
 	public static void main(String[] args) {
 		String input = new Scanner(System.in).nextLine();
 		input = new StringBuilder(input).reverse().toString();
