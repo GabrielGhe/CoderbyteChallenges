8ec98939ce62eddd41829c478651633fe91822cd
Gabriel G <GabrielGhe@users.noreply.github.com>
2014-04-13 20:41:16 -0400

    Create easy_LetterCapitalize.java

---
+++ easy_LetterCapitalize.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Easy_LetterCapitalize {
+
+	public static String function(String input){
+		String[] words = input.split(" "); //split into words
+		StringBuilder result = new StringBuilder(); //StringBuilder > String
+		for(String w : words){
+			String temp = w.toUpperCase(); //Make whole word uppercase
+			result.append(temp.charAt(0) + w.substring(1) + " ");
+		}
+		return result.deleteCharAt(result.length() - 1).toString(); // removing the last " "
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(function(new Scanner(System.in).nextLine()));
+	}
+}
